648c37b1a4b95b59276ddf5acdcd72e884c405ad
Dan Johnson <danimal.johnson@gmail.com>
2021-06-10 09:32:20 -0700

    Create basic app framework

---
+++ src/components/Deliveries.js
@@ -0,0 +1,14 @@
+import Delivery from './Delivery';
+
+const Deliveries = () => {
+  return (
+    <div>
+      <h2>Deliveries</h2>
+      <Delivery />
+      <Delivery />
+      <Delivery />
+    </div>
+  )
+}
+
+export default Deliveries
